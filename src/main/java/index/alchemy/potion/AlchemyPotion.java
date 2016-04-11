9cc391ded263157095f80510e64e03c671b20cd8
Tinkle01 <1021287060@qq.com>
2016-04-11 18:03:50 +0800

    test

--- src/main/java/index/alchemy/potion/AlchemyPotion.java
+++ src/main/java/index/alchemy/potion/AlchemyPotion.java
@@ -4,9 +4,12 @@ import index.alchemy.core.AlchemyResourceLocation;
 import index.alchemy.core.CommonProxy;
 import index.alchemy.core.AlchemyEventSystem;
 import index.alchemy.core.IPlayerTickable;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
 
 public class AlchemyPotion extends Potion {
 	
@@ -20,7 +23,13 @@ public class AlchemyPotion extends Potion {
 	@Override
 	public boolean isInstant() {
         return ready;
-        // test
+    }
+	
+	@Override
+    public void renderInventoryEffect(int x, int y, PotionEffect effect, Minecraft mc)
+    {
+        mc.getTextureManager().bindTexture(new ResourceLocation(""));
+        mc.currentScreen.drawTexturedModalRect(x + 6, y + 7, 0, 0, 18, 18);
     }
 	
 	@Override
