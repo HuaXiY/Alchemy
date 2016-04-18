a97899ba8360acae0a9f9b3035285c0f55bd6898
Mickeyxiami <qq931920447@gmail.com>
2016-04-19 07:19:44 +0800

    update AmuletGuard logic

--- src/main/java/index/alchemy/item/ItemAmuletGuard.java
+++ src/main/java/index/alchemy/item/ItemAmuletGuard.java
@@ -9,14 +9,14 @@ import index.alchemy.item.AlchemyItemBauble.AlchemyItemAmulet;
 
 public class ItemAmuletGuard extends AlchemyItemAmulet {
 	
-	public static final int RECOVERY_INTERVAL = 20 * 2, RECOVERY_CD = 20 * 5;
+	public static final int RECOVERY_INTERVAL = 20 * 2, RECOVERY_CD = 20 * 5, DURATION_TIME = 20 * 10;
 	
 	@Override
 	public void onWornTick(ItemStack item, EntityLivingBase living) {
 		if (living.ticksExisted % RECOVERY_INTERVAL == 0 && living.getLastAttackerTime() - living.ticksExisted > RECOVERY_CD) {
 			PotionEffect effect = living.getActivePotionEffect(MobEffects.ABSORPTION);
-			int amplifier = Math.min(effect == null ? 0 : effect.getAmplifier() + 1, 5);
-			living.addPotionEffect(new PotionEffect(MobEffects.ABSORPTION, 2, amplifier));
+			int amplifier = Math.min(effect == null ? 0 : effect.getAmplifier() + 1, 4);
+			living.addPotionEffect(new PotionEffect(MobEffects.ABSORPTION, DURATION_TIME, amplifier));
 		}
 	}
 
