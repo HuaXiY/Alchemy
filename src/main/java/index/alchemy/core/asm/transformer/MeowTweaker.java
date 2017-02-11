1ceba682f25caa330df6a7cabd72c7170664df2e
Mickeyxiami <xiami@mickey.moe>
2017-02-11 15:28:00 +0800

    update: Support for Java9

--- src/main/java/index/alchemy/core/asm/transformer/MeowTweaker.java
+++ src/main/java/index/alchemy/core/asm/transformer/MeowTweaker.java
@@ -1,13 +1,19 @@
 package index.alchemy.core.asm.transformer;
 
 import java.io.File;
+import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
 import java.util.List;
 
 import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.commons.GeneratorAdapter;
+import org.objectweb.asm.commons.Method;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import index.alchemy.core.AlchemyEngine;
+import index.alchemy.core.debug.AlchemyRuntimeException;
 import net.minecraft.launchwrapper.IClassTransformer;
 import net.minecraft.launchwrapper.ITweaker;
 import net.minecraft.launchwrapper.LaunchClassLoader;
@@ -25,11 +31,9 @@ public enum MeowTweaker implements ITweaker, IClassTransformer {
 					transformersField.setAccessible(true);
 					List<IClassTransformer> transformers = (List<IClassTransformer>) transformersField.get(loader);
 					transformers.add(this);
-				} catch(Exception e) { throw new RuntimeException(e); }
+				} catch(Exception e) { AlchemyRuntimeException.onException(e); }
 		}
 	};
-	
-	
 	
 	public static MeowTweaker instance() { return INSTANCE; }
 
@@ -51,11 +55,34 @@ public enum MeowTweaker implements ITweaker, IClassTransformer {
 
 	@Override
 	public byte[] transform(String name, String transformedName, byte[] basicClass) {
-		if (transformedName.equals("index.alchemy.util.ReflectionHelper")) {
-			ClassNode node = new ClassNode(ASM5);
-			ClassReader reader = new ClassReader(basicClass);
-			reader.accept(node, 0);
-		}
+		if (transformedName.equals("index.alchemy.util.ReflectionHelper"))
+			try {
+				ClassNode node = new ClassNode(ASM5);
+				ClassReader reader = new ClassReader(basicClass);
+				reader.accept(node, 0);
+				MethodNode target = null;
+				for (MethodNode method : node.methods)
+					if (method.name.equals("setAccessible") &&
+							method.desc.equals("(Ljava/lang/reflect/AccessibleObject;)Ljava/lang/reflect/AccessibleObject;"))
+						target = method;
+				if (target == null)
+					throw new RuntimeException(new NullPointerException("target"));
+				MethodNode newMethod = new MethodNode(target.access, target.name, target.desc, target.signature,
+						target.exceptions.toArray(new String[target.exceptions.size()]));
+				GeneratorAdapter adapter = new GeneratorAdapter(newMethod, target.access, target.name, target.desc);
+				Field overrideField = AccessibleObject.class.getDeclaredField("override");
+				long overrideFieldOffset = AlchemyEngine.unsafe().objectFieldOffset(overrideField);
+				adapter.loadArg(0);
+				adapter.dup();
+				adapter.push(overrideFieldOffset);
+				adapter.push(1);
+				adapter.invokeStatic(Type.getType(AlchemyEngine.unsafe().getClass()), new Method("putBoolean", Type.getMethodDescriptor(
+						Type.VOID_TYPE, Type.getType(Object.class), Type.LONG_TYPE, Type.BOOLEAN_TYPE)));
+				adapter.returnValue();
+				adapter.endMethod();
+				node.methods.remove(target);
+				node.methods.add(newMethod);
+			} catch(Exception e) { AlchemyRuntimeException.onException(e); }
 		return basicClass;
 	}
 
