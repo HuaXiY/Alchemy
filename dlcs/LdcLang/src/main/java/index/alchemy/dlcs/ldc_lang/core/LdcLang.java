d3da466afd7b5d491bc8b4e8c2b810458e191514
Mickeyxiami <xiami@mickey.moe>
2017-03-19 06:29:52 +0800

    update

--- dlcs/LdcLang/src/main/java/index/alchemy/dlcs/ldc_lang/core/LdcLang.java
+++ dlcs/LdcLang/src/main/java/index/alchemy/dlcs/ldc_lang/core/LdcLang.java
@@ -4,15 +4,13 @@ import java.io.File;
 import java.io.IOException;
 
 import index.alchemy.api.annotation.DLC;
-import index.alchemy.api.annotation.Init;
 import index.alchemy.core.AlchemyModLoader;
-import net.minecraftforge.client.ClientCommandHandler;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.common.LoaderState.ModState;
+import net.minecraft.command.CommandBase;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import static index.alchemy.dlcs.ldc_lang.core.LdcLang.*;
 
-@Init(state = ModState.INITIALIZED)
 @DLC(id = DLC_ID, name = DLC_NAME, version = DLC_VERSION, mcVersion = "[1.10.2]")
 public class LdcLang {
 	
@@ -21,10 +19,8 @@ public class LdcLang {
 			DLC_NAME = "LdcLang",
 			DLC_VERSION = "0.0.1-dev";
 	
-	public static void init() {
-		if (FMLCommonHandler.instance().getSide().isClient())
-			ClientCommandHandler.instance.registerCommand(new CommandMakeLang());
-	}
+	@SideOnly(Side.CLIENT)
+	public static final CommandBase make_lang = new CommandMakeLang();
 	
 	static { loadLangMapping(); }
 
